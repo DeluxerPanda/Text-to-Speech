5cf764585d14dbcaf0f805d732c5deec96efaeba
DeluxerPanda <85413771+DeluxerPanda@users.noreply.github.com>
2022-09-17 16:11:39 +0200

    Add files via upload

---
+++ darkmode.js
@@ -0,0 +1,32 @@
+let darkMode = localStorage.getItem("darkMode");
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
+
+const EnabledDarkMode = () => {
+    document.body.classList.add("darkmode");
+
+    localStorage.setItem("darkMode", "enabled");
+}
+
+const DisabledDarkMode = () => {
+    document.body.classList.remove("darkmode");
+
+    localStorage.setItem("darkMode", null);
+}
+if (darkMode === "enabled") {
+    EnabledDarkMode();
+    document.getElementById("dark-mode-toggle").innerHTML = 'Swtich to Light mode';
+}else{
+    DisabledDarkMode();
+    document.getElementById("dark-mode-toggle").innerHTML = 'Swtich to Dark mode';
+}
+
+darkModeToggle.addEventListener("click", () => {
+    darkMode = localStorage.getItem("darkMode");
+    if (darkMode !== "enabled") {
+        document.getElementById("dark-mode-toggle").innerHTML = 'Swtich to Light mode';
+        EnabledDarkMode();
+    }else{
+        document.getElementById("dark-mode-toggle").innerHTML = 'Swtich to Dark mode';
+        DisabledDarkMode();
+    }
+});
